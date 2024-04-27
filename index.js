a87f7f2abff1cb9481412f7747beb4bcf5185a3c
Maria Allwin <149009714+Allwinlio10@users.noreply.github.com>
2024-04-27 22:40:45 +1400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function validate () {
+    let username = document.getElementById(username);
+    value;
+    let password = document.getElementById(password);
+    value;
+
+if( username === 'Mariaallwin' && password === 'allwinsam')
+{
+    alert('login successfully!');
+} 
+else{
+    alert('login failed!');
+}
+
+}
